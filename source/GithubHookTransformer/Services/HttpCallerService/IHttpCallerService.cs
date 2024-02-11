9d46c042a3123b0bfacecb6cc2daa367034d30b6
Mojtaba Tajik <onyfel@gmail.com>
2024-02-11 08:46:35 +0100

    Add Http Caller Service

---
+++ source/GithubHookTransformer/Services/HttpCallerService/IHttpCallerService.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace GithubHookTransformer.Services.HttpCallerService;
+
+public interface IHttpCallerService
+{
+    public Task PostPayloadAsync(string url, string jsonPayload);
+}
